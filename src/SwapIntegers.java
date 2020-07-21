aece199b21d140674443fbf76bf2a051baba2861
Natasha Pavri <natashapavri@gmail.com>
2020-07-21 16:54:34 -0700

    some more problems

---
+++ src/SwapIntegers.java
@@ -0,0 +1,22 @@
+
+public class SwapIntegers {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+				
+		System.out.println(a);
+		System.out.println(b);
+		
+		a = 5;
+		b = 6;
+		a = a^b;
+		b = a^b;
+		a = a^b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
