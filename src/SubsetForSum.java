d80cc27537468110c243d0bcb259dee4089e69bb
Natasha Pavri <natashapavri@gmail.com>
2020-07-22 22:08:14 -0700

    more problems

--- src/SubsetForSum.java
+++ src/SubsetForSum.java
@@ -1,39 +1,45 @@
+import java.util.HashMap;
+
 public class SubsetForSum {
 
-	public void findSubArray(int[] arrA, int Sum) {
-		int currSum = 0;
-		int start = 0;
-		for (int i = 0; i <= arrA.length; i++) {
+	public int[] findSubArray(int[] arrA, int Sum) {
 
-			while (currSum > Sum) {
-				// System.out.println(currSum + " reducing");
-				currSum = currSum - arrA[start++];
-				// System.out.println(currSum + " reduced");
+		HashMap<Integer, Integer> sums = new HashMap<>();
+		int prefixSum = 0;
+		int start = 0;
+		int end = -1;
+		for (int i = 0; i < arrA.length; i++) {
+			prefixSum += arrA[i];
+			if (prefixSum - Sum == 0) {
+				start = 0;
+				end = i;
+				break;
 			}
-			if (currSum == Sum) {
-				System.out.println(Sum + " is found between indexes " + start
-						+ " and " + (i - 1));
-				// print the exact elements
-				System.out.print("And Elements are : ");
-				for (int j = start; j <= i - 1; j++) {
-					System.out.print(" " + arrA[j]);
-				}
-				return;
+
+			if (sums.containsKey(prefixSum - Sum)) {
+				start = sums.get(prefixSum - Sum) + 1;
+				end = i;
+				break;
 			}
-			if (i < arrA.length)
-				currSum = currSum + arrA[i];
-			// System.out.println(currSum);
+
+			sums.put(prefixSum, i);
 		}
 
-		// if we have reached that means that we have not found the subarray
-		// with summation equals to Sum
-		System.out.println("No subarray is found with sum equals to " + Sum);
+		int[] result = new int[end - start + 1];
+		int i =0;
+		for (int j = start; j <= end; j++) {
+			result[i++] = arrA[j];
+		}
+		return result;
 	}
 
 	public static void main(String[] args) throws java.lang.Exception {
 		int[] arrA = { 25, 12, 14, 22, 19, 15, 10, 23 };
 		int Sum = 55;
 		SubsetForSum i = new SubsetForSum();
-		i.findSubArray(arrA, Sum);
+		int[] result  = i.findSubArray(arrA, Sum);
+		for(int j =0 ;j<result.length; j++) {
+			System.out.print(result[j] + ", ");
+		}
 	}
 }
